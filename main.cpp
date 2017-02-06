64ee695369c43ae1ff45042a7ff1d01e4a73df64
smeth194243 <smor7432@740s-pl-106.local>
2017-02-06 12:28:04 -0700

    ugh

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AfternoonMegaData
+//
+//  Created by Morris, Seth on 2/6/17.
+//  Copyright © 2017 Morris, Seth. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
