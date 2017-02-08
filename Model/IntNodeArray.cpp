2f2208bbd570b7c310d3ed5a7866250b65e2fbce
smeth194243 <smor7432@740s-pl-106.local>
2017-02-08 12:53:17 -0700

    Created IntNodeArray

--- Model/IntNodeArray.cpp
+++ Model/IntNodeArray.cpp
@@ -7,5 +7,20 @@
 //
 
 #include "IntNodeArray.hpp"
+#include <assert.h>
 
+IntNodeArray :: IntNodeArray(int size)
+{
+    assert(size > 0);
+    
+    this-> size = size;
+    this-> front = new IntNode();
+    
+    for(int index =0; index < size; index++)
+    {
+        IntNode * current = new IntNode();
+        current->setNodePointer(front);
+        front = current;
+    }
+}
 
