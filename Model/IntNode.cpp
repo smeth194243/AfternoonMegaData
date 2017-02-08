cad001f9e7bd409bc0459aa72dc4b48ed76182bf
smeth194243 <smor7432@740s-pl-106.local>
2017-02-08 12:11:20 -0700

    Added IntNode.cpp

--- Model/IntNode.cpp
+++ Model/IntNode.cpp
@@ -7,3 +7,41 @@
 //
 
 #include "IntNode.hpp"
+
+IntNode :: IntNode()
+{
+    this->nodePointer = nullptr;
+    this->nodeData = -9999;
+}
+
+IntNode :: IntNode(int value)
+{
+    this->nodePointer = nullptr;
+    this->nodeData = value;
+}
+
+IntNode :: IntNode(int value, IntNode * nodePointer)
+{
+    this->nodePointer = nodePointer;
+    this->nodeData = value;
+}
+
+void IntNode:: setNodeData(int newValue)
+{
+    nodeData = newValue;
+}
+
+void IntNode:: setNodePointer(IntNode * nodePointer)
+{
+    this->nodePointer = nodePointer;
+}
+
+int IntNode:: getNodeData()
+{
+    return nodeData;
+}
+
+IntNode * IntNode :: getNodePointer()
+{
+    return this->nodePointer;
+}
