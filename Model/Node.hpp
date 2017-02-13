2e2f3ffd1a3eca3b3cb4b8cfb71e01f978d96d8c
smeth194243 <smor7432@740s-pl-106.local>
2017-02-13 12:58:17 -0700

    MAKE. IT. STOP.

--- Model/Node.hpp
+++ Model/Node.hpp
@@ -26,4 +26,52 @@ public:
     Node<Type> * getNodePointer();
 };
 
+/*
+ Implementation section
+ */
+
+template <class Type>
+Node<Type> :: Node()
+{
+    //Explicitly DO NOT instantiate the nodeData data member
+    this-> nodePointer = nullptr;
+}
+
+template <class Type>
+Node<Type> :: Node(Type data)
+{
+    this-> nodeData = data;
+    this-> nodePointer = nullptr;
+}
+
+template <class Type>
+Node<Type> :: Node(Type nodeData, Node<Type> * nodePointer)
+{
+    this-> nodeData = data;
+    this-> nodePointer = next;
+}
+
+template <class Type>
+void Node<Type> :: setNodeData(Type data)
+{
+    this->nodeData = data;
+}
+
+template <class Type>
+void Node<Type> :: setNodePointer(Node<Type> * nextPointer)
+{
+    this->nodePointer = nextPointer;
+}
+
+template <class Type>
+Type Node<Type> :: getNodeData()
+{
+    return this-> nodeData;
+}
+
+template <class Type>
+Node<Type> * Node<Type> :: getNodePointer()
+{
+    return this-> nodePointer;
+}
 #endif /* Node_hpp */
