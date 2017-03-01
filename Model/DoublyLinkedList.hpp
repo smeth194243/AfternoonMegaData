db81d80046da76dc1f07c0bc4d464aac8f5bfd33
smeth194243 <smor7432@740s-pl-106.local>
2017-03-01 12:51:48 -0700

    Created D_L_L

---
+++ Model/DoublyLinkedList.hpp
@@ -0,0 +1,19 @@
+//
+//  DoublyLinkedList.hpp
+//  AfternoonMegaData
+//
+//  Created by Morris, Seth on 3/1/17.
+//  Copyright © 2017 Morris, Seth. All rights reserved.
+//
+
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+#include "BiDirectionalNode.hpp"
+
+template <class Type>
+class DoublyLinkedList
+{
+    
+};
+
+#endif /* DoublyLinkedList_h */
