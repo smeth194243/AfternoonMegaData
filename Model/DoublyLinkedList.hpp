3bc4fd146ec2d1e1a5a0d641a68acf7fd5ab31ea
smeth194243 <smor7432@740s-pl-106.local>
2017-03-01 13:09:24 -0700

    Worked on DLL.hpp

--- Model/DoublyLinkedList.hpp
+++ Model/DoublyLinkedList.hpp
@@ -13,7 +13,46 @@
 template <class Type>
 class DoublyLinkedList
 {
-    
+private:
+    BiDirectionalNode<Type> * front;
+    BiDirectionalNode<Type> * end;
+    int size;
+public:
+    DoublyLinkedList();
+    virtual ~DoublyLinkedList() = 0;
+    int getSize() const;
+    BiDirectionalNode<Type> * getFront() const;
+    BiDirectionalNode<Type> * getEnd() const;
+
+    virtual void add(Type data) = 0;
+    virtual Type remove(int index) = 0;
 };
+
+template <class Type>
+DoublyLinkedList<Type> :: DoublyLinkedList()
+{
+    size = 0;
+    front = nullptr;
+    end = nullptr;
+}
+
+template <class Type>
+int DoublyLinkedList<Type> :: getSize()
+{
+    return size;
+}
+
+template <class Type>
+BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getFront()
+{
+    return front;
+}
+
+template <class Type>
+BiDirectionalNode<Type> * DoublyLinkedList<Type> :: getEnd()
+{
+    return end;
+}
+
 
 #endif /* DoublyLinkedList_h */
