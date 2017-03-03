104e0597df7e38f347ec8192d2892e6f6d8b7afd
smeth194243 <smor7432@740s-pl-106.local>
2017-03-03 13:30:56 -0700

    More queueueueueueueueueue

--- Model/Queue.hpp
+++ Model/Queue.hpp
@@ -24,5 +24,37 @@ public:
     void enqueue(Type data);
     Type dequeue();
     Type peek();
+};
+
+template <class Type>
+void Queue<Type> :: add(Type value)
+{
+    enqueue(value);
+}
+
+/*
+ Add to Queue:
+ Create Node
+ If First - adjust front
+ else add to end
+ move end 
+ adjust size
+ */
+
+template <class Type>
+void Queue<Type> :: enqueue(Type insertedValue)
+{
+    BiDirectionalNode<Type> * added = BiDirectionalNode<Type>(insertedValue);
+    
+    if(this->getSize() == 0 || this->getFront() == nullptr || this->getEnd() == nullptr)
+    {
+        this->setFront(added);
+    }
+    else
+    {
+        this->getEnd()->setNextPointer(added);
+    }
+    this->setEnd(added);
+    this->setSize(this->getSize() + 1);
 }
 #endif /* Queue_h */
