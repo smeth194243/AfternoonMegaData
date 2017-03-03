103effb0a3566293613694000c7f5b7682e47865
smeth194243 <smor7432@740s-pl-106.local>
2017-03-03 13:09:23 -0700

    don't ask me whats going on, I don't know.

---
+++ Model/Queue.hpp
@@ -0,0 +1,13 @@
+//
+//  Queue.hpp
+//  AfternoonMegaData
+//
+//  Created by Morris, Seth on 3/3/17.
+//  Copyright © 2017 Morris, Seth. All rights reserved.
+//
+
+#ifndef Queue_h
+#define Queue_h
+
+#include "DoublyLinked 
+#endif /* Queue_h */
