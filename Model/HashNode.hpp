d51d5b3584ea9884cef4ad398e5cd2a8eaa8220c
smeth194243 <smor7432@740s-pl-106.local>
2017-05-01 12:54:54 -0600

    eeeeaaaaahhh

---
+++ Model/HashNode.hpp
@@ -0,0 +1,57 @@
+//
+//  HashNode.hpp
+//  AfternoonMegaData
+//
+//  Created by Morris, Seth on 5/1/17.
+//  Copyright © 2017 Morris, Seth. All rights reserved.
+//
+
+#ifndef HashNode_h
+#define HashNode_h
+
+template <class Type>
+class HashNode
+{
+private:
+    Type data;
+    long key;
+public:
+    HashNode();
+    HashNode(Type, data);
+    
+    Type getData();
+    long getKey() const;
+    void setData(Type data);
+};
+
+template <class Type>
+HashNode<Type> :: HashNode()
+{
+    this->key = rand();
+}
+
+template <class Type>
+HashNode<Type> :: HashNode(Type, data)
+{
+    this->key = rand();
+    this->data = data;
+}
+
+template <class Type>
+long HashNode<Type> :: getKey() const
+{
+    return this->key;
+}
+
+template <class Type>
+Type HashNode<Type> :: getData()
+{
+    return this->data;
+}
+
+template <class Type>
+void HashNode<Type> :: setData(Type data)
+{
+    this->data = data;
+}
+#endif /* HashNode_h */
--- Model/Hashtable.hpp
+++ Model/Hashtable.hpp
@@ -37,5 +37,86 @@ public:
     void displayContents();
 };
 
+template <class Type>
+HashTable<Type> :: HashTable()
+{
+    this->capacity= 101;
+    this->efficiencyPercentage = 0.667;
+    this->size = 0;
+    this->hashTableStorage = HashNode<Type> * [capacity];
+}
+
+template <class Type>
+HashTable<Type> :: ~HashTable()
+{
+    delete [ ] hashTableStorage;
+}
+
+template <class Type>
+int HashTable<Type> :: getNextPrime()
+{
+    int nextPrime = (this->capacity * 2) +1;
+    
+    while(!isPrime(nextPrime))
+    {
+        nextPrime += 2;
+    }
+    return nextPrime;
+}
+
+template <class Type>
+bool HashTable<Type> :: isPrime(long canidateNumber)
+{
+    if(canidateNumber <= 1)
+    {
+        return false;
+    }
+    else if(canidateNumber == 2|| canidateNumber == 3)
+    {
+        return true;
+    }
+    else if(canidateNumber % 2 == 0)
+    {
+        return false;
+    }
+    else
+    {
+        for(int next = 3; next <= sqrt(canidateNumber) +1; next +=2)
+        {
+            if(canidateNumber % next = 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+template <class Type>
+long HashTable<Type> :: findPosition(HashNode<Type> * data)
+{
+    long insertPosition = data->getKey() % this->capacity;
+    return insertposition;
+}
+
+template <class Type>
+long HashTable<Type> :: handleCollision(HashNode<Type> * data, long currentPosition)
+{
+    long shift = 17;
+    
+    for(long position = currentPosition + shift; position != currentPosition; position += shift)
+    {
+        if(position >= capacity)
+        {
+            position = position % capacity;
+        }
+        
+        if(hashTableStorage[position] == nullptr)
+        {
+            return position;
+        }
+    }
+    return -1;
+}
 
 #endif /* Hashtable_h */
