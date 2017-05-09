168989efb9b6a083be7292a16a9b9e5f50d22919
smeth194243 <smor7432@740s-pl-106.local>
2017-05-09 12:22:07 -0600

    graph

---
+++ Model/Graph.h
@@ -0,0 +1,64 @@
+//
+//  Graph.h
+//  AfternoonMegaData
+//
+//  Created by Morris, Seth on 5/9/17.
+//  Copyright © 2017 Morris, Seth. All rights reserved.
+//
+
+#ifndef Graph_h
+#define Graph_h
+
+#include <set>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 20;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> graph, int vertex, bool markedVertices[])
+    
+public:
+    Graph();
+    ~Graph();
+    
+    void addVertex(const Type& value);
+    void addEdge(int source, int target);
+    void removeEdge(int source, int target);
+    Type& operator [ ] (int vertex) const;
+    Type operator [] (int vertex) const;
+    int size() const;
+    bool areConnected(int source, int target) const;
+    std::set<int> neighbors(int vertex) const;
+    void depthFirstTraversal(Graph<Type> graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> graph, int vertex);
+};
+
+template <class Type>
+const int Graph<Type> :: MAXIMUM;
+
+template <class Type>
+Graph<Type> :: Graph()
+{
+    this->vertexCount = 0;
+}
+
+template <class Type>
+Graph<Type> :: ~Graph()
+{
+    delete [] this->adjacencyMatrix;
+    delete [] this->graphData;
+}
+
+template <class Type>
+int Graph<Type> :: size() const
+{
+    return vertexCount;
+}
+#endif /* Graph_h */
